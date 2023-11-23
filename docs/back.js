8fc379287faaed8809ccce5f7cbac25fb1176d4d
Ferman <Ferman557@gmail.com>
2023-11-24 02:38:07 +0400

    color modification and dark mode icon

--- docs/back.js
+++ docs/back.js
@@ -1,17 +1,44 @@
 //r = root
 //rs = root style
+//r.style to setproperty
+//rs.getproperty to getproperty
 var r = document.querySelector(':root');
 
 function changetheme(){
     rs = getComputedStyle(r);
     if(rs.getPropertyValue("--bg-color") == "#85c1e9"){
-        r.style.setProperty("--bg-color", "#1f1f1f");
-        r.style.setProperty("--font-color", "#D07AD6");
-       // r.style.setProperty("--element-bg-color", "#85c1e9");
+        r.style.setProperty("--bg-color", "#323645");
+        r.style.setProperty("--font-color", "#AE00FF");
+        r.style.setProperty("--element-bg-color", "#B9E09D");
 
     }else{
         r.style.setProperty("--bg-color", "#85c1e9");
         r.style.setProperty("--font-color", "#000000");
-        //r.style.setProperty("--element-bg-color", "#85c1e9");
+        r.style.setProperty("--element-bg-color", "#000000");
     }
 }
+/*
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); */
