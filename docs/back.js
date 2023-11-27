1a74bb046aaddd240c9a1d1e77b996516fb2606e
Ferman <Ferman557@gmail.com>
2023-11-27 13:49:09 +0400

    Color for both themes (website done)

--- docs/back.js
+++ docs/back.js
@@ -1,44 +1,66 @@
+var mycolor = localStorage.getItem("PageColor");
+
 //r = root
 //rs = root style
 //r.style to setproperty
 //rs.getproperty to getproperty
 var r = document.querySelector(':root');
+checkcolor();
 
+function checkcolor(){
+  if(mycolor === "dark"){
+    r.style.setProperty("--bg-color", "#323645"); //very dark blue
+    r.style.setProperty("--font-color", "#8338ec");//purple font color
+    r.style.setProperty("--element-bg-color", "#B9E09D");//lime element color
+  }else{
+    r.style.setProperty("--bg-color", "#f3efe2");
+    r.style.setProperty("--font-color", "#FFFFFF");
+    r.style.setProperty("--element-bg-color", "#90b8b7");
+  }
+}
 function changetheme(){
-    rs = getComputedStyle(r);
-    if(rs.getPropertyValue("--bg-color") == "#85c1e9"){
-        r.style.setProperty("--bg-color", "#323645");
-        r.style.setProperty("--font-color", "#AE00FF");
-        r.style.setProperty("--element-bg-color", "#B9E09D");
-
+    //rs = getComputedStyle(r);
+    if(localStorage.getItem("PageColor") === "light"){
+        localStorage.setItem("PageColor", "dark");
+        r.style.setProperty("--bg-color", "#323645"); //very dark blue
+        r.style.setProperty("--font-color", "#8338ec");//purple font color
+        r.style.setProperty("--element-bg-color", "#B9E09D");//lime element color
     }else{
-        r.style.setProperty("--bg-color", "#85c1e9");
-        r.style.setProperty("--font-color", "#000000");
-        r.style.setProperty("--element-bg-color", "#000000");
+      localStorage.setItem("PageColor", "light");
+        r.style.setProperty("--bg-color", "#f3efe2");
+        r.style.setProperty("--font-color", "#FFFFFF");
+        r.style.setProperty("--element-bg-color", "#90b8b7");
     }
 }
-/*
-var nodemailer = require('nodemailer');
+//Copy number and email to clipboard with in site notification*************
+function copynum(){
+  navigator.clipboard.writeText("971561412252");
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
-  }
-});
+  showSnackBar();
+}
+function copymail(){
+  navigator.clipboard.writeText("3abdulla.Almazrouei@gmail.com");
 
-var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+  showSnackBar();
+}
+function showSnackBar() {
+  var sb = document.getElementById("snackbar");
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-}); */
+  //this is where the class name will be added & removed to activate the css
+  sb.className = "show";
+
+  setTimeout(()=>{ sb.className = sb.className.replace("show", ""); }, 3000);
+}
+function SendEmail(){
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "myfermanbusiness@gmail.com",
+    Password : "5886B29FD82BAF062751DA151211B104BA82",
+    To : 'fermaninquires@gmail.com',
+    From : "myfermanbusiness@gmail.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => console.log(message)
+);
+}
